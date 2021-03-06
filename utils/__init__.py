1ef81d98557dd47f23a292e9cc86c216afde64d0
SijunXu <francis_xu@icloud.com>
2021-03-06 19:55:06 +0800

    fix bug for import error

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .parallel import make_parallel
 from .preprocess_data import round_phi, round_points, IRC_cut
 from .lorentz import get_p4
-from .data_utils import ML_data
+from .data_utils import RawData, ML_data
